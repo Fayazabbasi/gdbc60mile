c3e20d1f9786abd848ab1154151ad5a66b2e8c11
Fayaz Hussain <fayazabbasi498@gmail.com>
2026-09-01 22:42:56 +0500

    Diagnose Laravel Vercel startup

--- api/index.php
+++ api/index.php
@@ -1,3 +1,21 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+echo "PHP is working<br>";
+echo "PHP Version: " . PHP_VERSION . "<br>";
+
+echo "Project root: " . dirname(__DIR__) . "<br>";
+
+if (!file_exists(__DIR__ . '/../public/index.php')) {
+    die("ERROR: public/index.php not found");
+}
+
+if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    die("ERROR: vendor/autoload.php not found");
+}
+
+echo "Laravel files found<br>";
+
 require __DIR__ . '/../public/index.php';
